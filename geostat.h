f55493d402e9dfb50dd543cbe37917aeede0161a
Emard <vordah@gmail.com>
2025-07-26 17:14:43 +0200

    ESP32 crash after connecting to GPS. Still out of memory. Reducing default to 100 snap points. Currently no crash.

--- geostat.h
+++ geostat.h
@@ -36,7 +36,7 @@
 #define hash_grid_size 32 // N*N grid 32*32=1024
 // snap_point_max is largest memory hog. If set too large,
 // SD card will not mount or esp32 will reboot at open SD log files
-#define snap_point_max 250 // (Tools->PSRAM: Disabled) total max snap points up to 25 km
+#define snap_point_max 100 // (Tools->PSRAM: Disabled) total max snap points up to 10 km
 //#define snap_point_max 1000 // (Tools->PSRAM: Enabled) total max snap points up to 100 km
 
 //extern int wr_snap_ptr;
