81555f9692724a9291cc4b3227b35fa9ea54bf7c
davor <davor@ssd.lan>
2025-08-06 19:03:06 +0200

    wav2nmea.py extract NMEA tags

---
+++ tools/wav2nmea.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+# apt install python3-fastkml python3-shapely python3-lxml
+# ./wav2kml.py 20210701.wav > 20210701.kml
+
+# TODO option to colorize with calc values
+
+from sys import argv
+from functools import reduce
+from operator import xor
+
+# buffer to read wav
+b=bytearray(12)
+mvb=memoryview(b)
+
+for wavfile in argv[1:]:
+  i = 0
+  f = open(wavfile, "rb")
+  seek = 44+0*12
+  f.seek(seek)
+  nmea=bytearray(0)
+  while f.readinto(mvb):
+    seek += 12 # bytes per sample
+    a=(b[0]&1) | ((b[2]&1)<<1) | ((b[4]&1)<<2) | ((b[6]&1)<<3) | ((b[8]&1)<<4) | ((b[10]&1)<<5)
+    if a != 32:
+      c = a
+      # convert control chars<32 to uppercase letters >=64
+      if((a & 0x20) == 0):
+        c ^= 0x40
+      if a != 33:
+        nmea.append(c)
+    else: # a == 32
+      if(len(nmea)):
+        print(nmea.decode("utf-8"))
+      # delete, consumed
+      nmea=bytearray(0)
+    i += 1
+  f.close()
