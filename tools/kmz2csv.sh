33d7363108fb963ca3025c45a3d022ab38afef7c
davor <davor@brix16.lan>
2025-08-22 12:23:40 +0200

    csv script runnable from any directory

--- tools/kmz2csv.sh
+++ tools/kmz2csv.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+BASEDIR=$(dirname $0)
+
 for x in $*
 do
   unzip $x
   mv doc.kml $x.kml
-  xsltproc kml2csv.xsl $x.kml > $x.csv
+  xsltproc $BASEDIR/kml2csv.xsl $x.kml > $x.csv
   rm $x.kml
 done
