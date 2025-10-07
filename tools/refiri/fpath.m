a0b78ca35a037d18ec19e252bf9e5fb77d57d534
Davor Emard <vordah@gmail.com>
2025-10-07 11:20:44 +0200

    octave test for wave function

---
+++ tools/refiri/fpath.m
@@ -0,0 +1,27 @@
+function y = fpath(x)
+% wave function: x [m] = f(y [m]) 
+
+% each example makes a profile with IRI=1 mm/m:
+
+% y = +0.92e-3*sin(2*pi*x);
+% clean sine 1 m wavelength
+% without 5-point average makes IRI = 1
+% with 5-point average makes IRI = 0.9
+
+y = +1.07e-3*sin(2*pi*0.2*x);
+% clean sine 5 m wavelength
+
+% y = +2.116e-3*sin(2*pi*0.09*x);
+% clean sine 11.111 m wavelength
+
+% y = +2.1e-3*sin(2*pi*0.09*x+0.01/0.0111111*sin(2*pi*0.0111111*x));
+% wobbling sine around 11.111 m wavelength
+% along pathlen_m IRI varies around 1.00+-0.05 with period 90 m
+
+% y = +2.1e-3*sin(2*pi*0.09*x+0.01/0.0111111*sin(2*pi*0.0111111*x)) +0.1e-3*sin(2*pi*4.50*x+0.00/0.0526315*sin(2*pi*0.0526315*x));
+% 0.1e-3*sin() has negligible contribution
+% superposition of two wobbling sines:
+% first wobbles around 11.111 m wavelength and second wobbles around 0.222222 m wavelength
+% along pathlen_m IRI varies around 1.00+-0.05 with period 90 m
+
+end % function
---
+++ tools/refiri/testfpath.m
@@ -0,0 +1,32 @@
+#!/usr/bin/octave
+
+% reads profile as complex vector (x,y*i)
+dx=0.05; % [m] sampling step
+% dx=x_m(1234)-x_m(1233); % should be the same
+%L=20; % [m] for IRI20
+L=100; % [m] for IRI100
+%L=500; % [m] for IRI500
+N=round(L/dx); % number of 5-cm points to average for IRI
+for i = 1:N
+  x_m(i)=i*dx;
+  y_mm(i)=1000*fpath(x_m(i)); % [m] -> [mm]
+end
+
+% invoking iri() loads file iri.m and uses function iri(prof,dx)
+iri_value=iri(y_mm,dx);
+% internal iri()'s running average of 5 points for 25-cm average
+% output vector "iri_value" is M shorter than input vector "y_mm":
+% M=5 actually
+M=length(y_mm)-length(iri_value)
+printf('x[m]   IRI%d[mm/m]\n', L);
+printf('-------------------\n');
+for i = N:N:length(x_m)
+  % disp([x_m(i),iri_value(i-5)]);
+  % -M because there is moving sum of M values each 5 cm spaced - 5 makes avg of 25 cm
+  N1=N; % N1 is similar to N but avoids first negative index
+  if i == N N1=N-M; end
+  printf('%7.2f %10.6f\n', x_m(i), sum(iri_value(i-(N1+M-1):i-M))/N1);
+  % disp([i-(N1+M-1) i-M N1]);
+end
+%disp(length(x_m))
+%disp(length(iri_value))
