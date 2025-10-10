d6b5b1ee57bf635a089dfa39432f6a3f0ce1011d
Emard <vordah@gmail.com>
2025-10-10 12:05:16 +0200

    kmz2csv one-liner

--- tools/kmz2csv.sh
+++ tools/kmz2csv.sh
@@ -4,10 +4,11 @@ BASEDIR=$(dirname $0)
 
 for x in $*
 do
-  unzip $x
-  mv doc.kml $x.kml
-  xsltproc $BASEDIR/kml2csv.xsl $x.kml > $x.csv
-  rm $x.kml
+  #unzip $x
+  #mv doc.kml $x.kml
+  #xsltproc $BASEDIR/kml2csv.xsl $x.kml > $x.csv
+  #rm $x.kml
+  unzip -qc $x | xsltproc $BASEDIR/kml2csv.xsl - > $(dirname $x)/$(basename $x .kmz).csv
 done
 
 # rename 's/\.kmz.csv$/\.csv/' /tmp/data/*.kmz.csv
