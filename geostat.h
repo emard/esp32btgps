2a54a1a6bee8f286e37f4efbc56675a8bd98ae7d
Emard <vordah@gmail.com>
2025-10-16 17:42:05 +0200

    max points 5000, up to 500 km every 100 m

--- geostat.h
+++ geostat.h
@@ -44,7 +44,7 @@
 // snap_point_max is largest memory hog. If set too large,
 // SD card will not mount or esp32 will reboot at open SD log files
 //#define snap_point_max 50 // (Tools->PSRAM: Disabled) total max snap points up to 5 km
-#define snap_point_max 2000 // (Tools->PSRAM: Enabled) total max snap points up to 200 km
+#define snap_point_max 5000 // (Tools->PSRAM: Enabled) total max snap points up to 500 km
 
 //extern int wr_snap_ptr;
 extern int lat2grid,  lon2grid;
